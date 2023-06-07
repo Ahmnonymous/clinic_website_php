b160c5aeaf3506b1d62de0d0102c04726e1aaa6f
ahmnonymous <a4medqureshi8@gmail.com>
2023-06-07 14:50:29 +0500

    css updates and links

--- index.php
+++ index.php
@@ -41,10 +41,10 @@ if (isset($_POST['submit'])) {
     <title>Ehsan Clinic</title>
 
     <!-- JQuery link  -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/jquery-3.6.0.min.js"></script>
 
     <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
