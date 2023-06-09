7abcde53c321f50c4478f674ce724bbf9ae175d6
ahmnonymous <a4medqureshi8@gmail.com>
2023-06-10 03:43:11 +0500

    cla na

---
+++ local-config.php
@@ -0,0 +1,14 @@
+<?php
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'a4medqureshi8@gmail.com');
+define('SMTP_PASSWORD', 'rribxwdfhxybqhhp');
+define('SMTP_AUTH', 'true');
+define('SMTP_SECURE', 'tls');
+define('SMTP_AUTO_TLS', 'true');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'db_contact_php');
+define('DB_HOST', 'localhost');
+
+?>
